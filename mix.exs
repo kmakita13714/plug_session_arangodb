9f793e87d70b6780c521d10f8a9230a5085ba903
Ko-ichi MAKITA <makita-a@nihon-e.co.jp>
2020-01-29 19:54:13 +0900

    - Update version number

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PlugSessionArangodb.MixProject do
     [
       app: :plug_session_arangodb,
       build_embedded: Mix.env() == :prod,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
